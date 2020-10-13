2fc975027460b417939b25ab4e2f8a916b6c7f00
Svetlana <gubinas83@gmail.com>
2020-10-13 21:29:01 +0300

    feat: implement decode function

--- src/index.js
+++ src/index.js
@@ -1,46 +1,77 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-': 'a',
+  '-...': 'b',
+  '-.-.': 'c',
+  '-..': 'd',
+  '.': 'e',
+  '..-.': 'f',
+  '--.': 'g',
+  '....': 'h',
+  '..': 'i',
+  '.---': 'j',
+  '-.-': 'k',
+  '.-..': 'l',
+  '--': 'm',
+  '-.': 'n',
+  '---': 'o',
+  '.--.': 'p',
+  '--.-': 'q',
+  '.-.': 'r',
+  '...': 's',
+  '-': 't',
+  '..-': 'u',
+  '...-': 'v',
+  '.--': 'w',
+  '-..-': 'x',
+  '-.--': 'y',
+  '--..': 'z',
+  '.----': '1',
+  '..---': '2',
+  '...--': '3',
+  '....-': '4',
+  '.....': '5',
+  '-....': '6',
+  '--...': '7',
+  '---..': '8',
+  '----.': '9',
+  '-----': '0',
+};
+
+const SPACE = '**********';
+const LEN = 10;
+
+const digitsToLetter = (str) => {
+  const DOT = '10';
+  const DASH = '11';
+
+  let digits = str.match(new RegExp('.{1,' + 2 + '}', 'g'));
+  let result = '';
+  for (let i = 0; i < digits.length; i++) {
+    if (digits[i] === DOT) {
+      result += '.';
+    } else if (digits[i] === DASH) {
+      result += '-';
+    }
+  }
+
+  return MORSE_TABLE[result];
 };
 
 function decode(expr) {
-    // write your solution here
+  let encodedLetters = expr.match(new RegExp('.{1,' + LEN + '}', 'g'));
+
+  let finalString = '';
+  for (let i = 0; i < encodedLetters.length; i++) {
+    if (encodedLetters[i] === SPACE) {
+      finalString += " ";
+    } else {
+      finalString += digitsToLetter(encodedLetters[i]);
+    }
+  }
+
+  return finalString;
 }
 
 module.exports = {
-    decode
+  decode
 }
